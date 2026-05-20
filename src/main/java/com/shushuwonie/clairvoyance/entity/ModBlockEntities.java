05d9689fa98a6ea6aca14b059494a876744d455e
shushuwonie <3203818760@qq.com>
2026-05-20 19:11:20 +0800

    Add files via upload

---
+++ src/main/java/com/shushuwonie/clairvoyance/entity/ModBlockEntities.java
@@ -0,0 +1,60 @@
+package com.shushuwonie.clairvoyance.entity;
+
+import com.shushuwonie.clairvoyance.Clairvoyance;
+import com.shushuwonie.clairvoyance.features.block.BodyPartBlockEntity;
+import com.shushuwonie.clairvoyance.features.block.arm.LeftArmBlockEntity;
+import com.shushuwonie.clairvoyance.features.block.arm.RightArmBlockEntity;
+import com.shushuwonie.clairvoyance.features.block.leg.LeftLegBlockEntity;
+import com.shushuwonie.clairvoyance.features.block.leg.RightLegBlockEntity;
+import com.shushuwonie.clairvoyance.features.block.torso.TorsoBlockEntity;
+import com.shushuwonie.clairvoyance.item.modblock.ModBlocks;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import java.util.Set;
+
+public class ModBlockEntities {
+    public static final BlockEntityType<TorsoBlockEntity> TORSO_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of(Clairvoyance.MOD_ID, "torso"),
+            FabricBlockEntityTypeBuilder.create(TorsoBlockEntity::new, ModBlocks.TORSO).build()
+    );
+
+    public static final BlockEntityType<LeftArmBlockEntity> LEFT_ARM_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of(Clairvoyance.MOD_ID, "left_arm"),
+            FabricBlockEntityTypeBuilder.create(LeftArmBlockEntity::new, ModBlocks.LEFT_ARM).build()
+    );
+
+    public static final BlockEntityType<RightArmBlockEntity> RIGHT_ARM_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of(Clairvoyance.MOD_ID, "right_arm"),
+            FabricBlockEntityTypeBuilder.create(RightArmBlockEntity::new, ModBlocks.RIGHT_ARM).build()
+    );
+    public static final BlockEntityType<LeftLegBlockEntity> LEFT_LEG_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of(Clairvoyance.MOD_ID, "left_leg"),
+            FabricBlockEntityTypeBuilder.create(LeftLegBlockEntity::new, ModBlocks.LEFT_LEG).build()
+    );
+    public static final BlockEntityType<RightLegBlockEntity> RIGHT_LEG_BLOCK_ENTITY = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier.of(Clairvoyance.MOD_ID, "right_leg"),
+            FabricBlockEntityTypeBuilder.create(RightLegBlockEntity::new, ModBlocks.RIGHT_LEG).build()
+    );
+
+
+
+
+
+
+
+
+
+
+    public static void initialize() {
+        // 触发静态初始化
+    }
+}
